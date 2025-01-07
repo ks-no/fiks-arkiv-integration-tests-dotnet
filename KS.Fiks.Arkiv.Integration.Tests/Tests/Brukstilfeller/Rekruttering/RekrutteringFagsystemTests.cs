354d50de9436efadc3ae6baba015ed39ac2c82e8
Jarle Børsheim <jarle.borsheim@sonat.no>
2025-01-07 09:23:45 +0100

    Test for innstillingsvedtak for rekrutteringssystemer

--- KS.Fiks.Arkiv.Integration.Tests/Tests/Brukstilfeller/Rekruttering/RekrutteringFagsystemTests.cs
+++ KS.Fiks.Arkiv.Integration.Tests/Tests/Brukstilfeller/Rekruttering/RekrutteringFagsystemTests.cs
@@ -50,7 +50,7 @@ namespace KS.Fiks.Arkiv.Integration.Tests.Tests.Brukstilfeller.Rekruttering
          * OBS: Denne oppretter mappen som brukes i testene som følger
          */
         [Test, Order(1)]
-        public async Task Opprett_Ledig_Stilling_Saksmappe()
+        public async Task A_Opprett_Ledig_Stilling_Saksmappe()
         {
             // Denne id'en gjør at Arkiv-simulatoren ser hvilke meldinger som henger sammen. Har ingen funksjon ellers.  
             var testSessionId = Guid.NewGuid().ToString();
@@ -146,11 +146,11 @@ namespace KS.Fiks.Arkiv.Integration.Tests.Tests.Brukstilfeller.Rekruttering
     
           /*
          * EasyCruit sitt eksempel er brukt i denne testen, mtp regel osv.
-         * Denne testen oppretter journalposten med hoveddokument for en ledig stilling på saksmappe opprettet tidligere
+         * Denne testen oppretter journalposten for en ledig stilling på saksmappe opprettet tidligere
          * Til slutt henter testen opprettet journalpost og validerer den
          */
         [Test, Order(2)]
-        public async Task Opprett_Ledig_Stilling_Journalpost_Med_HovedDokument_På_Saksmappen()
+        public async Task B_Opprett_Ledig_Stilling_Journalpost_Med_HovedDokument_På_Saksmappen()
         {
             // Denne id'en gjør at Arkiv-simulatoren ser hvilke meldinger som henger sammen. Har ingen funksjon ellers.  
             var testSessionId = Guid.NewGuid().ToString();
@@ -166,7 +166,7 @@ namespace KS.Fiks.Arkiv.Integration.Tests.Tests.Brukstilfeller.Rekruttering
             // Dokumentbeskrivelse med dokumentobjekt for hoveddokument
             var dokumentbeskrivelse = 
                 DokumentbeskrivelseBuilder.Init()
-                    .WithTittel("Integrasjonstest leseprøve vår 2023")
+                    .WithTittel("Prosjektdokument - ref-number-123")
                     .WithDokumenttype("DOKUMENT", "Dokument")
                     .WithDokumentstatus("F", "Dokumentet er ferdigstilt")
                     .WithTilknyttetRegistreringSom("H", "Hoveddokument")
@@ -200,8 +200,8 @@ namespace KS.Fiks.Arkiv.Integration.Tests.Tests.Brukstilfeller.Rekruttering
             var referanseEksternNoekkelNyJournalpost = GenererEksternNoekkel();
 
             var journalpost = JournalpostBuilder.Init()
-                .WithTittel($"Test fra {FagsystemNavn} - Prosjektdokument - ref-number-123!")
-                .WithOffentligTittel($"Test fra {FagsystemNavn} - Prosjektdokument - ref-number-123!")
+                .WithTittel($"Test fra {FagsystemNavn} - Prosjektdokument - ref-number-123")
+                .WithOffentligTittel($"Test fra {FagsystemNavn} - Prosjektdokument - ref-number-123")
                 .WithEksternNoekkel(referanseEksternNoekkelNyJournalpost)
                 .WithReferanseTilForelderMappe(new ReferanseTilMappe()
                 {
@@ -294,7 +294,157 @@ namespace KS.Fiks.Arkiv.Integration.Tests.Tests.Brukstilfeller.Rekruttering
             Assert.That(registreringHentResultat.Registrering.ReferanseEksternNoekkel.Fagsystem == arkivmelding.Registrering.ReferanseEksternNoekkel.Fagsystem);
             Assert.That(registreringHentResultat.Registrering.ReferanseEksternNoekkel.Noekkel == arkivmelding.Registrering.ReferanseEksternNoekkel.Noekkel);
         }
+        
+        /*
+         * EasyCruit sitt eksempel er brukt i denne testen, mtp regel osv.
+         * Denne testen oppretter journalposten med innstillingsvedtak for en ledig stilling på saksmappe opprettet tidligere
+         * Til slutt henter testen opprettet journalpost og validerer den
+         */
+        [Test, Order(3)]
+        public async Task C_Opprett_Innstillingsvedtak_Journalpost_Med_HovedDokument_På_Saksmappen()
+        {
+            // Denne id'en gjør at Arkiv-simulatoren ser hvilke meldinger som henger sammen. Har ingen funksjon ellers.  
+            var testSessionId = Guid.NewGuid().ToString();
+            
+            // Regel for dette usecaset!
+            ArkivmeldingRegel = "VEC-NOMINATION-LIST";
+
+            /*
+             * STEG 1:
+             * Opprett arkivmelding med journalpost og send til arkiv
+             */
+            
+            // Dokumentbeskrivelse med dokumentobjekt for hoveddokument
+            var dokumentbeskrivelse = 
+                DokumentbeskrivelseBuilder.Init()
+                    .WithTittel("Innstillingsvedtak - ref-number-123")
+                    .WithDokumenttype("VEDTAK", "Vedtak")
+                    .WithDokumentstatus("F", "Dokumentet er ferdigstilt")
+                    .WithTilknyttetRegistreringSom("H", "Hoveddokument")
+                    .WithDokumentobjekt(new Dokumentobjekt()
+                    {
+                        Versjonsnummer = 1,
+                        Variantformat = new Variantformat()
+                        {
+                            KodeProperty = "P",
+                            Beskrivelse = "Produksjonsformat"
+                        },
+                        Format = new Format()
+                        {
+                            KodeProperty = "RA-PDF"
+                        },
+                        Filnavn = "hoveddokument-test-vedlegg.pdf",
+                        ReferanseDokumentfil = "test-vedlegg.pdf"
+                    })
+                    .Build();
+            
+            var korrespondansepart = new Korrespondansepart();
+            korrespondansepart.KorrespondansepartNavn = "Navn Navnesen";
+            korrespondansepart.Epostadresse = "Navn.Navnesen@etellerannet.no";
+            korrespondansepart.ErBehandlingsansvarlig = false;
+            korrespondansepart.Korrespondanseparttype = new Korrespondanseparttype()
+            {
+                KodeProperty = "IA",
+                Beskrivelse = "Intern avsender"
+            };
+
+            var referanseEksternNoekkelNyJournalpost = GenererEksternNoekkel();
+
+            var journalpost = JournalpostBuilder.Init()
+                .WithTittel($"Test fra {FagsystemNavn} - Innstillingsvedtak - ref-number-123")
+                .WithOffentligTittel($"Test fra {FagsystemNavn} - Innstillingsvedtak - ref-number-123")
+                .WithEksternNoekkel(referanseEksternNoekkelNyJournalpost)
+                .WithReferanseTilForelderMappe(new ReferanseTilMappe()
+                {
+                    ReferanseEksternNoekkel = _saksmappeEksternNoekkel // Refererer til saksmappen opprettet tidligere
+                })
+                .WithJournalposttype("X", "Organinternt dokument uten oppfølging")
+                .WithJournalstatus("A")
+                .WithDokumentbeskrivelse(dokumentbeskrivelse)
+                .WithKorrespondansepart(korrespondansepart)
+                .Build();
+
+            MottattMeldingArgs? arkivmeldingKvitteringMelding;
+            PayloadFile arkivmeldingKvitteringPayload;
+
+
+            var arkivmelding = MeldingGenerator.CreateArkivmelding(FagsystemNavn);
+            arkivmelding.Registrering = journalpost;
+
+            var nyJournalpost = SerializeHelper.Serialize(arkivmelding);
+            
+            // Utkommenter dette hvis man vil å skrive til fil for å sjekke resultat manuelt
+            // File.WriteAllText("RekrutteringMedJournalpostDokumentOgRegel.xml", nyJournalpost);
+            
+            // Valider arkivmelding
+            validator.Validate(nyJournalpost);
+
+            // Send melding
+            var meldingAttachment = new MeldingAttachment
+            {
+                Filename = "test-vedlegg.pdf",
+                Filestream = new FileStream("test-vedlegg.pdf", FileMode.Open, FileAccess.Read, FileShare.None)
+            };
+            var attachments = new List<MeldingAttachment> { meldingAttachment };
+            var nySaksmappeMeldingId = await FiksRequestService.Send(MottakerKontoId, FiksArkivMeldingtype.ArkivmeldingOpprett, nyJournalpost, attachments, testSessionId);
+            
+            // Vent på 2 første response meldinger (mottatt og kvittering)
+            VentPaSvar(2, 10);
+            Assert.That(MottatMeldingArgsList.Count > 0, "Fikk ikke noen meldinger innen timeout");
+
+            // Verifiser at man får mottatt og arkivmelding kvittering meldinger tilbake
+            SjekkForventetMeldinger(MottatMeldingArgsList, nySaksmappeMeldingId, new []{FiksArkivMeldingtype.ArkivmeldingOpprettMottatt, FiksArkivMeldingtype.ArkivmeldingOpprettKvittering});
+
+            // Hent meldingen
+            arkivmeldingKvitteringMelding = GetMottattMelding(MottatMeldingArgsList, nySaksmappeMeldingId, FiksArkivMeldingtype.ArkivmeldingOpprettKvittering);
+            
+            arkivmeldingKvitteringPayload = MeldingHelper.GetDecryptedMessagePayload(arkivmeldingKvitteringMelding).Result;
+            Assert.That(arkivmeldingKvitteringPayload.Filename == "arkivmelding-kvittering.xml", "Filnavn ikke som forventet arkivmelding-kvittering.xml");
     
+            // Valider innhold (xml)
+            validator.Validate(arkivmeldingKvitteringPayload.PayloadAsString);
+
+            /*
+             * STEG 2:
+             * Hent oppprettet journalpost igjen og valider
+             * 
+             */
+            var registreringHent = RegistreringHentBuilder.Init().WithEksternNoekkel(referanseEksternNoekkelNyJournalpost).Build();
+            
+            var registreringHentAsString = SerializeHelper.Serialize(registreringHent);
+            
+            // Valider innhold (xml)
+            validator.Validate(registreringHentAsString);
+            
+            // Nullstill meldingsliste
+            MottatMeldingArgsList.Clear();
+            
+            // Send hent melding
+            var hentMeldingId = await FiksRequestService.Send(MottakerKontoId, FiksArkivMeldingtype.RegistreringHent, registreringHentAsString, null, testSessionId);
+
+            // Vent på 1 respons meldinger 
+            VentPaSvar(1, 10);
+
+            Assert.That(MottatMeldingArgsList.Count > 0, "Fikk ikke noen meldinger innen timeout");
+            
+            // Verifiser at man får MappeHentResultat melding
+            SjekkForventetMelding(MottatMeldingArgsList, hentMeldingId, FiksArkivMeldingtype.RegistreringHentResultat);
+            
+            // Hent meldingen
+            var hentResultatMelding = GetMottattMelding(MottatMeldingArgsList, hentMeldingId, FiksArkivMeldingtype.RegistreringHentResultat);
+
+            Assert.That(hentResultatMelding != null);
+            
+            var hentResultatPayload = MeldingHelper.GetDecryptedMessagePayload(hentResultatMelding).Result;
+            
+            // Valider innhold (xml)
+            validator.Validate(hentResultatPayload.PayloadAsString);
+
+            var registreringHentResultat = SerializeHelper.DeserializeXml<RegistreringHentResultat>(hentResultatPayload.PayloadAsString);
+
+            Assert.That(registreringHentResultat.Registrering.ReferanseEksternNoekkel.Fagsystem == arkivmelding.Registrering.ReferanseEksternNoekkel.Fagsystem);
+            Assert.That(registreringHentResultat.Registrering.ReferanseEksternNoekkel.Noekkel == arkivmelding.Registrering.ReferanseEksternNoekkel.Noekkel);
+        }
         
     }
 }
